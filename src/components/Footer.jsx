fe14f1b84da2354a06dfb8aee8d6d3410f20ca26
Bukunmi Akinyemi <bkakinyemi21@gmail.com>
2025-02-16 19:29:08 +0100

    footer adjust

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <section className="mt-8 tablet:mt-10 tablet:text-body-m">
       <hr className="w-full border-gray-2 dark:border-black-4" />
-      <p className="text-gray text-center text-body-s m-4">A product of CaseSimpli Legal Padi</p>
+      <p className="text-gray text-center text-body-s m-4">A product of CaseSimpli Legal Research</p>
     </section>
   )
 }
